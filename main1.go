46a1411ac00bc698833cf7a3d88419c52d071532
Arun Nagaraja <arun.nagaraja@quickheal.com>
2022-07-06 15:33:31 +0530

    new commit

--- main1.go
+++ main1.go
@@ -4,6 +4,9 @@ import ("fmt")
 func main() {
   
   a :="this is another test for pull"
+  b :=10;
+  fmt.Println("b value is", b+10)
   fmt.Println("Hi Arun ", a);
   fmt.Println("this is another hotfix test from Branching")
+  fmt.Println("this is inside main branch")
 }
